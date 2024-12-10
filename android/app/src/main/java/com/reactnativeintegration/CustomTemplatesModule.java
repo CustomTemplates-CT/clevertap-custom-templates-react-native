20b76d74bc76ccfd4c6ace5d40c1b4e5fd0a4ddb
Karthik-CT <karthik.iyer@clevertap.com>
2024-12-10 11:39:42 +0530

    added tooltips nd support

--- android/app/src/main/java/com/reactnativeintegration/CustomTemplatesModule.java
+++ android/app/src/main/java/com/reactnativeintegration/CustomTemplatesModule.java
@@ -3,8 +3,11 @@ package com.reactnativeintegration;
 import android.os.Handler;
 import android.os.Looper;
 import android.util.Log;
+
 import androidx.appcompat.app.AppCompatActivity;
+
 import com.clevertap.ct_templates.nd.coachmark.CoachMarkHelper;
+import com.clevertap.ct_templates.nd.tooltips.TooltipHelper;
 import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
@@ -25,31 +28,6 @@ public class CoachMarkModule extends ReactContextBaseJavaModule {
 
     @ReactMethod
     public void showCoachMarks(String jsonString, Callback onComplete) {
-//        AppCompatActivity activity = (AppCompatActivity) getCurrentActivity();
-//        if (activity == null) {
-//            Log.e("CoachMarkModule", "Current activity is null");
-//            onComplete.invoke("Activity is null"); // Pass error back to JavaScript
-//            return;
-//        }
-//
-//        try {
-//            JSONObject jsonObject = new JSONObject(jsonString);
-//            CoachMarkHelper coachMarkHelper = new CoachMarkHelper();
-//            coachMarkHelper.renderCoachMark(activity, jsonObject, new kotlin.jvm.functions.Function0<kotlin.Unit>() {
-//                @Override
-//                public kotlin.Unit invoke() {
-//                    if (onComplete != null) {
-//                        onComplete.invoke(); // Notify JavaScript callback
-//                    }
-//                    return kotlin.Unit.INSTANCE;
-//                }
-//            });
-//        } catch (Exception e) {
-//            Log.e("CoachMarkModule", "Error rendering coach marks: " + e.getMessage());
-//            onComplete.invoke("Error rendering coach marks: " + e.getMessage()); // Pass error back
-//        }
-
-
         new Handler(Looper.getMainLooper()).post(new Runnable() {
             @Override
             public void run() {
@@ -65,6 +43,39 @@ public class CoachMarkModule extends ReactContextBaseJavaModule {
                     JSONObject jsonObject = new JSONObject(jsonString);
                     CoachMarkHelper coachMarkHelper = new CoachMarkHelper();
                     coachMarkHelper.renderCoachMark(activity, jsonObject, new kotlin.jvm.functions.Function0<kotlin.Unit>() {
+                        @Override
+                        public kotlin.Unit invoke() {
+                            if (onComplete != null) {
+                                onComplete.invoke(); // Notify JavaScript callback
+                            }
+                            return kotlin.Unit.INSTANCE;
+                        }
+                    });
+                } catch (Exception e) {
+                    Log.e("CoachMarkModule", "Error rendering coach marks: " + e.getMessage());
+                    onComplete.invoke("Error rendering coach marks: " + e.getMessage()); // Pass error back
+                }
+            }
+        });
+    }
+
+    @ReactMethod
+    public void showTooltips(String jsonString, Callback onComplete) {
+        new Handler(Looper.getMainLooper()).post(new Runnable() {
+            @Override
+            public void run() {
+                // Perform your UI updates here
+                AppCompatActivity activity = (AppCompatActivity) getCurrentActivity();
+                if (activity == null) {
+                    Log.e("CoachMarkModule", "Current activity is null");
+                    onComplete.invoke("Activity is null"); // Pass error back to JavaScript
+                    return;
+                }
+
+                try {
+                    JSONObject jsonObject = new JSONObject(jsonString);
+                    TooltipHelper tooltipHelper = new TooltipHelper();
+                    tooltipHelper.showTooltips(activity, jsonObject, new kotlin.jvm.functions.Function0<kotlin.Unit>() {
                         @Override
                         public kotlin.Unit invoke() {
                             if (onComplete != null) {
