a8db5a2c96eb2be48be99f1e12ca6e0bdb5d0123
Karthik-CT <karthik.iyer@clevertap.com>
2025-09-08 22:27:59 +0530

    checked aar and iOS

--- ios/ReactNativeIntegration/AppDelegate.h
+++ ios/ReactNativeIntegration/AppDelegate.h
@@ -1,9 +1,8 @@
 #import <React/RCTBridgeDelegate.h>
 #import <UIKit/UIKit.h>
 #import <UserNotifications/UserNotifications.h>
-#import <CleverTap-iOS-SDK/CleverTapURLDelegate.h>
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate, RCTBridgeDelegate, CleverTapURLDelegate>
+@interface AppDelegate : UIResponder <UIApplicationDelegate, RCTBridgeDelegate>
 
 @property (nonatomic, strong) UIWindow *window;
 @property (nonatomic,strong) NSDictionary *resp;
