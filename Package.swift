f5bfc2fdd0d74e6f0a6948bc4b283d42f336279a
Karthik-CT <karthik.iyer@clevertap.com>
2025-09-13 14:17:35 +0530

    updated library

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.0
+// // swift-tools-version: 6.0
 // import PackageDescription
 
 // let package = Package(
@@ -21,21 +21,31 @@
 //     ]
 // )
 
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+// swift-tools-version: 5.7
+// swift-tools-version:5.7
+// swift-tools-version:5.7
+import PackageDescription
+
 let package = Package(
-    name: "clevertap-custom-templates-react-native",
-    platforms: [.iOS(.v13)],
+    name: "CTCustomTemplates",
+    platforms: [.iOS(.v13)], // Ensure iOS support is specified
     products: [
         .library(
-            name: "clevertap-custom-templates-react-native",
+            name: "CTCustomTemplates",
             targets: ["CTCustomTemplates"]
         ),
     ],
     targets: [
         .target(
             name: "CTCustomTemplates",
-            path: "Sources/CTCustomTemplates",
-            resources: [
-                .process("Assets")
+            dependencies: [],
+            path: "CTCustomTemplates/Sources/CTCustomTemplates",
+            swiftSettings: [.define("SWIFT_PACKAGE")], // Helps avoid import issues
+            linkerSettings: [
+                .linkedFramework("UIKit") // ✅ Ensure UIKit is linked
             ]
         )
     ]
