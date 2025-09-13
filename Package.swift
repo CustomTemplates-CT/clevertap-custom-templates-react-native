fd943fb8346dacb6a1deab620ad29f2f0abfabbb
Karthik-CT <karthik.iyer@clevertap.com>
2025-09-13 13:23:05 +0530

    updated library

--- Package.swift
+++ Package.swift
@@ -1,12 +1,32 @@
 // swift-tools-version: 6.0
-import PackageDescription
+// import PackageDescription
+
+// let package = Package(
+//     name: "CTCustomTemplates",
+//     platforms: [.iOS(.v13)], // Ensure iOS support is specified
+//     products: [
+//         .library(
+//             name: "CTCustomTemplates",
+//             targets: ["CTCustomTemplates"]
+//         ),
+//     ],
+//     targets: [
+//         .target(
+//             name: "CTCustomTemplates",
+//             path: "Sources/CTCustomTemplates",
+//             resources: [
+//                 .process("Assets")
+//             ]
+//         )
+//     ]
+// )
 
 let package = Package(
-    name: "CTCustomTemplates",
-    platforms: [.iOS(.v13)], // Ensure iOS support is specified
+    name: "clevertap-custom-templates-react-native",
+    platforms: [.iOS(.v13)],
     products: [
         .library(
-            name: "CTCustomTemplates",
+            name: "clevertap-custom-templates-react-native",
             targets: ["CTCustomTemplates"]
         ),
     ],
